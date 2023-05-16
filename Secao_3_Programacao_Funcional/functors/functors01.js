7e0be7957011c0ea382b7f5e49dc68e64b1b7f3a
BrunoColturato <brcolturato@gmail.com>
2023-05-15 21:17:51 -0300

    Functors study

---
+++ Secao_3_Programacao_Funcional/functors/functors01.js
@@ -0,0 +1,23 @@
+/**
+ * Functors sao objetos que implementam uma funcao MAP.
+ * Tambem sao um encapsulamento de valores.
+ * Quando o MAP eh chamado, retorna-se um objeto do mesmo
+ * tipo do functor.
+ *
+ * Exemplo: Array.
+ */
+
+function TipoSeguro(valor) {
+  return {
+    valor,
+    invalido() {
+      return this.valor === null || this.valor === undefined;
+    },
+    map(fn) {
+      return this.invalido() ? TipoSeguro(null) : TipoSeguro(fn(this.valor));
+    },
+  };
+}
+
+const resultado = TipoSeguro("Esse eh um texto").map((t) => t.toUpperCase());
+console.log(resultado);
